c07db80c359aa21215571d6c4d6b982b96d4bed4
Bert Jansen <bjansen@microsoft.com>
2015-09-08 11:14:39 +0200

    rename

--- InfoPath/Samples/KnockOut.SinglePageApp/KnockOut.SinglePageApp/Assets/Emp-EditForm.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-JSRequest.EnsureSetup();
-var displayMode = JSRequest.QueryString["DisplayMode"];
-
-if (typeof displayMode == "undefined") {
-    var itemId = JSRequest.QueryString["ID"];
-    window.location = _spPageContextInfo.webAbsoluteUrl + "/SitePages/EmpForm.aspx?itemId="+itemId;
-}
-</script>
--- InfoPath/Samples/KnockOut.SinglePageApp/KnockOut.SinglePageApp/Assets/Emp-NewForm.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-JSRequest.EnsureSetup();
-var displayMode = JSRequest.QueryString["DisplayMode"];
-
-if (typeof displayMode == "undefined") {
-    window.location = _spPageContextInfo.webAbsoluteUrl + "/SitePages/EmpForm.aspx";
-}
-</script>
